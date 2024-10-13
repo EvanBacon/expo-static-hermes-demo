13e2de811be412ac33f7bb78e59b8c940d8dcb35
evanbacon <bacon@expo.io>
2024-10-13 16:17:38 -0500

    working

---
+++ metro.config.js
@@ -0,0 +1,14 @@
+// Learn more https://docs.expo.io/guides/customizing-metro
+const { getDefaultConfig } = require("expo/metro-config");
+
+/** @type {import('expo/metro-config').MetroConfig} */
+const config = getDefaultConfig(__dirname);
+
+config.transformer.getTransformOptions = async () => ({
+  transform: {
+    experimentalImportSupport: true,
+    inlineRequires: false,
+  },
+});
+
+module.exports = config;
